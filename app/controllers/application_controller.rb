b3ddd561e9b657304ba14cc1c4b077d0a264786d
vsafonov <vsafonov@d-a-c.ru>
2017-02-12 13:57:36 +0300

    Set response for html queries

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :dispatch_responder
+
+  private
+
+  def dispatch_responder
+    respond_to do |format|
+      format.json
+      format.html { render 'spa/index' }
+    end
+  end
 end
